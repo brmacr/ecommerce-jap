cb1cebbc25ba1fc0271975217ce138d7b377c73e
brmacr <deicris96@hotmail.com>
2021-08-22 21:16:38 -0300

    Productos
    Modifique archivo products.html y products.js para mostrar lograr mostrar PRODUCTOS.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,45 @@
+var productsArray = [];
+
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let products = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + products.imgSrc + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ products.name +`</h4>
+                        <small class="text-muted">` + products.soldCount +  ` vendidos</small>
+                    </div>
+                    <p>`+ products.description +`</p>
+                    <small class="text-muted" >` + products.currency + ` ` + products.cost + `</small>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("productos_muchos").innerHTML=htmlContentToAppend;
+    }
+}   
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e){
+    showSpinner();
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            //Muestro los productos ordenados
+            showProductsList(productsArray);
+        }
+    hideSpinner();
+    });
 });
