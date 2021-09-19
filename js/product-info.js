cc5271d6c4325db651c82315d14c7e21779b549b
brmacr <deicris96@hotmail.com>
2021-09-19 18:53:47 -0300

    products.js
    Peticiones a url para mostrar info de productos.

--- js/product-info.js
+++ js/product-info.js
@@ -1,6 +1,93 @@
+let product = {};
+let comentariosLindos = {};
+
+function showImagesGallery(array) {
+
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < array.length; i++) {
+        let imageSrc = array[i];
+
+        htmlContentToAppend += `
+        <div class="col-lg-3 col-md-4 col-6">
+            <div class="d-block mb-4 h-100">
+                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+            </div>
+        </div>
+        `
+
+        document.getElementById("productosLocos").innerHTML = htmlContentToAppend;
+    }
+}
+
+function mostrarComentarios() {
+    let htmlContentToAppend = "";
+
+    for (let i = 0; i < comentariosLindos.length; i++) {
+        let estrellasEndemoniadas = "";
+        let com = comentariosLindos[i];
+
+        for (let i = 0; i < com.score; i++) {
+            estrellasEndemoniadas += ` <span class="fa fa-star checked"></span>`;
+        };
+
+        for (let i = 0; i < 5 - com.score; i++) {
+            estrellasEndemoniadas += ` <span class="fa fa-star"></span>`;
+        };
+
+        htmlContentToAppend += `
+    <div class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col">
+                <p>`+ com.description + `</p>
+                <div class="d-flex w-100 justify-content-between">
+                <small>` + com.user + `</small>
+                </div>
+                <small>` + estrellasEndemoniadas + `</small><br>
+                <small class="text-muted" >` + com.dateTime + `</small>
+            </div>
+        </div>
+    </div>
+    `
+    }
+    document.getElementById("comentarios").innerHTML = htmlContentToAppend;
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            product = resultObj.data;
+
+
+            let productNameHTML = document.getElementById("productName");
+            let productDescriptionHTML = document.getElementById("productDescription");
+            let productSoldCountHTML = document.getElementById("productSoldCount");
+            let productCurrencyHTML = document.getElementById("productCurrency");
+            let productCategoryHTML = document.getElementById("productCategory");
+            let usuario = document.getElementById("usuario");
+
+
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productSoldCountHTML.innerHTML = product.soldCount;
+            productCurrencyHTML.innerHTML = product.currency + " " + product.cost;
+            productCategoryHTML.innerHTML = product.category;
+            usuario.value = localStorage.getItem("correo");
+
+
+            //Muestro las imagenes en forma de galería
+            showImagesGallery(product.images);
+        }
+
+    });
+    getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            comentariosLindos = resultObj.data;
+            mostrarComentarios();
+        }
+    })
 
 });
